ce8f362a2bd5da277430ce66b837d113a6e5af0b
BacharMetar <metar@post.bgu.ac.il>
2025-01-05 21:56:06 +0200

    add some logic for expenses

--- Roomiebill.Server/Facades/GroupFacade.cs
+++ Roomiebill.Server/Facades/GroupFacade.cs
@@ -16,6 +16,12 @@ namespace Roomiebill.Server.Facades
         {
             Groups.Add(group);
         }
+        //create a new group
+        public void CreateGroup(int id,string name, string description, User admin)
+        {
+            Group group = new Group(id, name, description, admin);
+            Groups.Add(group);
+        }
 
         public void InviteToGroupByUsername(int inviter_id, string invited_username, int groupId)
         {
