cf7cfb4c21a69d4a8146573497eb4629efab0b8a
vladik1998 <shembel.vladik@gmail.com>
2025-01-12 00:37:34 +0200

    something that runs

--- FrontendApplication/Platforms/Android/MainActivity.cs
+++ FrontendApplication/Platforms/Android/MainActivity.cs
@@ -1,11 +1,21 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Util;
 
 namespace FrontendApplication
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme",MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // This is a line where you can set a breakpoint
+            Log.Debug("MainActivity", "MainActivity is starting...");
+
+            // Add other initialization code here if needed
+        }
     }
 }
