116619de78b3b09c8a34cc92b8d4c5f2a20505e0
TalKoren11 <kotal@post.bgu.ac.il>
2025-01-14 13:38:34 +0200

    added status and Invite constractor.

--- Roomiebill.Server/Models/Invite.cs
+++ Roomiebill.Server/Models/Invite.cs
@@ -1,13 +1,34 @@
 
+using Roomiebill.Server.Enums;
+
 namespace Roomiebill.Server.Models
 {
     public class Invite
     {
         public int Id { get; set; }
-        public User Inviter { get; set; }
-        public User Invitee { get; set; }
-        public Group Group { get; set; }
-        // public Status Status { get; set; }
+        public int InviterId { get; set; }
+        public int InviteeId { get; set; }
+        public int GroupId { get; set; }
+        public Status Status { get; set; }
         public DateTime Date { get; set; }
+
+        public Invite(int inviterId, int inviteeId, int groupId)
+        {
+            InviterId = inviterId;
+            InviteeId = inviteeId;
+            GroupId = groupId;
+            Status = Status.Pending;
+            Date = DateTime.Now;
+        }
+
+        public void AcceptInvite()
+        {
+            Status = Status.Accepted;
+        }
+
+        public void RejectInvite()
+        {
+            Status = Status.Rejected;
+        }
     }
 }
