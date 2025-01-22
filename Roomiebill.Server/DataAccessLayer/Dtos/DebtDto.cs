fbade99c69d706c19c17b5596f230bf5a84c4dbf
BacharMetar <metar@post.bgu.ac.il>
2025-01-22 02:21:47 +0200

    update applicationdb

--- Roomiebill.Server/DataAccessLayer/Dtos/DebtDto.cs
+++ Roomiebill.Server/DataAccessLayer/Dtos/DebtDto.cs
@@ -2,5 +2,7 @@ public class DebtDto
 {
     public int OwedByUserId { get; set; }
     public string OwedByUserName { get; set; } // Optional: For user display
+    public int OwedToUserId { get; set; }
+    public string OwedToUserName { get; set; } // Optional: For user display
     public double Amount { get; set; }
 }
--- Roomiebill.Server/Models/Group.cs
+++ Roomiebill.Server/Models/Group.cs
@@ -27,7 +27,7 @@ namespace Roomiebill.Server.Models
 
         [JsonIgnore]
         public List<Invite> Invites { get; set; }
-        
+
         [NotMapped]
         [JsonIgnore]
         ExpenseHandler expenseHandler { get; set; }
@@ -39,14 +39,16 @@ namespace Roomiebill.Server.Models
             Members = new List<User>();
             Invites = new List<Invite>();
             GroupName = "Default Name";
-            _debtArray = new int[1];
             Expenses = new List<Expense>();
             expenseHandler = new ExpenseHandler(Members);
+            InitializeDebtArray();
         }
+
+
 
         public Group(string groupName, User groupAdmin, List<User> members)
         {
-             if (string.IsNullOrWhiteSpace(groupName))
+            if (string.IsNullOrWhiteSpace(groupName))
                 throw new ArgumentException("Group name cannot be empty or null.", nameof(groupName));
 
             if (groupAdmin == null)
@@ -57,23 +59,66 @@ namespace Roomiebill.Server.Models
             Members = new List<User>(members); // Defensive copy
             Members.Add(groupAdmin); // Add the admin to the members list
             Invites = new List<Invite>();
-            _debtArray = new int[(Members.Count * (Members.Count - 1)) / 2]; // Calculate size for 1D representation
             Expenses = new List<Expense>();
             expenseHandler = new ExpenseHandler(Members);
+            InitializeDebtArray();
         }
 
+        private void InitializeDebtArray()
+        {
+            int memberCount = Members.Count;
+            int size = (memberCount * (memberCount - 1)) / 2; // Calculate size for 1D representation
+            _debtArray = new int[size];
+        }
 
-       private void EnlargeDebtArraySize(int newUserCount,int oldUserCount)
+        public void UpdateDebtArray()
+        {
+            // Reset the debt array
+            InitializeDebtArray();
+
+            // Update the debt array based on the current expenses
+            foreach (var expense in Expenses)
+            {
+                foreach (var split in expense.ExpenseSplits)
+                {
+                    int payerIndex = Members.ToList().FindIndex(m => m.Id == expense.PayerId);
+                    int userIndex = Members.ToList().FindIndex(m => m.Id == split.UserId);
+                    if (payerIndex != -1 && userIndex != -1 && payerIndex != userIndex)
+                    {
+                        int amount = (int)(split.Percentage * expense.Amount / 100);
+                        expenseHandler.UpdateDebtBetweenIndex(payerIndex, userIndex, amount, _debtArray);
+                    }
+                }
+            }
+        }
+
+        public void InitializeNonPersistentProperties()
+        {
+            expenseHandler = new ExpenseHandler(Members);
+            UpdateDebtArray();
+        }
+
+        public void AddExpense(Expense expense)
+        {
+            Expenses.Add(expense);
+            UpdateDebtArray();
+        }
+        // public void AddExpense(Expense expense)
+        // {
+        //     expenseHandler.AddExpense(expense, _debtArray);
+        //     Expenses.Add(expense);
+        // }
+        private void EnlargeDebtArraySize(int newUserCount, int oldUserCount)
         {
             // Copy existing data to the new array
-            _debtArray = expenseHandler.EnlargeDebtArraySize(newUserCount,oldUserCount,_debtArray);
+            _debtArray = expenseHandler.EnlargeDebtArraySize(newUserCount, oldUserCount, _debtArray);
         }
 
         public void AddMember(User user)
         {
             Members.Add(user);
             // Update the debt array size
-            EnlargeDebtArraySize(Members.Count,Members.Count-1);
+            EnlargeDebtArraySize(Members.Count, Members.Count - 1);
 
         }
 
@@ -83,21 +128,22 @@ namespace Roomiebill.Server.Models
             Members.AddRange(newMembers);
             int newUserCount = Members.Count;
             // Update the debt array size
-            EnlargeDebtArraySize(newUserCount,oldUserCount);
+            EnlargeDebtArraySize(newUserCount, oldUserCount);
         }
-        
-        private void ReduceDebtArraySize(int newUserCount, int oldUserCount, List<int> removedUsers){
 
-            _debtArray = expenseHandler.ReduceDebtArraySize(newUserCount,oldUserCount,removedUsers,_debtArray); 
+        private void ReduceDebtArraySize(int newUserCount, int oldUserCount, List<int> removedUsers)
+        {
+
+            _debtArray = expenseHandler.ReduceDebtArraySize(newUserCount, oldUserCount, removedUsers, _debtArray);
         }
 
         public void RemoveMember(User user)
         {
             Members.Remove(user);
-             List <int> removedUsers = new List<int>();
+            List<int> removedUsers = new List<int>();
             removedUsers.Add(user.Id);
             // Update the debt array size
-            ReduceDebtArraySize(Members.Count,Members.Count+1,removedUsers);
+            ReduceDebtArraySize(Members.Count, Members.Count + 1, removedUsers);
         }
 
         public void AddInvite(Invite invite)
@@ -105,38 +151,66 @@ namespace Roomiebill.Server.Models
             Invites.Add(invite);
         }
 
-         public void RemoveMembers(List<User> removedMembers){
-            List <int> removedUsers = new List<int>();
-            foreach(User user in removedMembers){
+        public void RemoveMembers(List<User> removedMembers)
+        {
+            List<int> removedUsers = new List<int>();
+            foreach (User user in removedMembers)
+            {
                 Members.Remove(user);
                 removedUsers.Add(user.Id);
             }
             // Update the debt array size
-            ReduceDebtArraySize(Members.Count,Members.Count+removedMembers.Count,removedUsers);
+            ReduceDebtArraySize(Members.Count, Members.Count + removedMembers.Count, removedUsers);
         }
         //TODO: check for operation completion
-        public void AddExpense(Expense expense){
-            expenseHandler.AddExpense(expense,_debtArray);
-            Expenses.Add(expense);
-        }
-        public void updateExpense(Expense oldExpense, Expense newExpense){
+        
+        public void updateExpense(Expense oldExpense, Expense newExpense)
+        {
             Expense updatedExpense = expenseHandler.UpdateExpense(oldExpense, newExpense, _debtArray);
             bool flag = Expenses.Remove(oldExpense);
             Expenses.Add(updatedExpense);
-        }   
+        }
         //TODO: check for operation completion
-        public void DeleteExpense(Expense expense){
-            expenseHandler.DeleteExpense(expense,_debtArray);
+        public void DeleteExpense(Expense expense)
+        {
+            expenseHandler.DeleteExpense(expense, _debtArray);
             Expenses.Remove(expense);
         }
-        public int [] getDebtArray(){
+        public int[] getDebtArray()
+        {
             return _debtArray;
-        }   
-        public int getDebtBetweenUsers(int user1Id, int user2Id){
-            return expenseHandler.GetDebtBetweenIndex(user1Id,user2Id,_debtArray);
         }
-
-       
+        public int getDebtBetweenUsers(int user1Id, int user2Id)
+        {
+            return expenseHandler.GetDebtBetweenIndex(user1Id, user2Id, _debtArray);
+        }
+        /// <summary>
+        /// calculate the debts owed to a specific user
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public List<DebtDto> GetDebtsForUser(int userId)
+        {
+            List<DebtDto> debts = new List<DebtDto>();
+            foreach (User member in Members)
+            {
+                if (member.Id != userId)
+                {
+                    int amount = expenseHandler.GetDebtBetweenIndex(member.Id, userId, _debtArray);
+                    if (amount > 0)
+                    {
+                        DebtDto debt = new DebtDto();
+                        debt.OwedByUserId = member.Id;
+                        debt.OwedToUserId = userId;
+                        debt.Amount = amount;
+                        debt.OwedByUserName = member.Username;
+                        debt.OwedToUserName = Admin.Username;
+                        debts.Add(debt);
+                    }
+                }
+            }
+            return debts;
+        }
 
         public User GetAdmin()
         {
