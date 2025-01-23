f653565aa540ef9e6632f09accdf36a6aa21503a
inbarbc <benchain@post.bgu.ac.il>
2025-01-23 15:50:56 +0200

    merge from main

--- Roomiebill.Server/Models/Group.cs
+++ Roomiebill.Server/Models/Group.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
-using Roomiebill.Server.DataAccessLayer.Dtos;
 using Roomiebill.Server.Facades;
 
 namespace Roomiebill.Server.Models
