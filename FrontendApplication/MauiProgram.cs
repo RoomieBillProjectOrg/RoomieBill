78bf3a98ceb28a7c584e100eb5fb56de1c4028ea
TalKoren11 <kotal@post.bgu.ac.il>
2025-04-03 15:17:20 +0300

    everything works :)

--- FrontendApplication/MauiProgram.cs
+++ FrontendApplication/MauiProgram.cs
@@ -27,7 +27,7 @@ namespace FrontendApplication
             // Configure Https
             var baseUrl = new Uri(AppConfig.ApiBaseUrl);
 #if ANDROID
-            baseUrl = new Uri("https://132.73.84.62/api");
+            baseUrl = new Uri("https://132.73.84.62:433/api");
 #endif
             // Register the HttpClient with a platform-specific base address
             builder.Services.AddSingleton<HttpClientService>();
